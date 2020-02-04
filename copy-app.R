de70365fd4ff961479ee00bb30ad71b0bd46715b
Sarah McDonald <sm9dv@virginia.edu>
2020-02-04 19:34:01 +0000

    minor spelling error previous version

--- copy-app.R
+++ copy-app.R
@@ -28,7 +28,7 @@ wheel <- function(col, radius = 1, ...)
 wheel(theme_Palette)
 
 ##### DATA #####
-responses <- read.csv("https://raw.githubusercontent.com/uva-bi-sdad/Measuring-STW-App/sarah/data-discovery-jan-20.csv", sep = ",",stringsAsFactors = FALSE, header=TRUE, encoding="UTF-8")
+responses <- read.csv("https://raw.githubusercontent.com/uva-bi-sdad/Measuring-STW-App/sarah/data-discovery-feb-3.csv", sep = ",",stringsAsFactors = FALSE, header=TRUE, encoding="UTF-8")
 names(responses) <- stri_trim(gsub("..Yes.No.|i\\.e\\..+or\\.|i\\.e\\..+|\\.{53}.+|\\.+", " ", names(responses)), side = "right")
 
 ##### FORM VARIABLES #####
